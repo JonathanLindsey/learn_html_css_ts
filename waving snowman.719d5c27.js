7ce043184f8b8fc75445a0d8e8bcdb750462d59b
JonathanLindsey <jonathanilindsey@gmail.com>
2022-06-18 02:24:09 +0000

    Deploying to gh-pages from @ JonathanLindsey/learn_html_css_ts@a83c989b96539d40f80673a9989aafb761e8b796 🚀

---
+++ waving snowman.719d5c27.js
@@ -0,0 +1,2 @@
+class vector_math{static deg_to_rad(t){return t*(Math.PI/180)}static vectorToXY(t,a,r,e){let c=vector_math.deg_to_rad(r);return[Math.cos(c)*e+t,-1*Math.sin(c)*e+a]}}
+//# sourceMappingURL=waving snowman.719d5c27.js.map
---
+++ waving snowman.b9f86ba0.js
@@ -0,0 +1,2 @@
+function setup(){createCanvas(400,400)}var angle=40,dir=!1;function draw(){background(220),rect(0,350,400,50),ellipse(200,300,150,150),ellipse(200,200,100,100),ellipse(200,120,75,75),line(160,200,100,150);let[e,l]=vector_math.vectorToXY(240,200,angle,78);line(240,200,e,l),dir?angle>=40?dir=!1:angle+=1:angle<=0?dir=!0:angle-=1}
+//# sourceMappingURL=waving snowman.b9f86ba0.js.map
